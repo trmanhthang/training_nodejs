a40254a9d157a353fe634cb86838f274ce456e81
trmanhthang <thangmt868@gmail.com>
2023-12-12 14:02:58 +0700

    update middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const middleware = require('./security/middleware')
 
 const route = require('./routes');
 
+app.use(middleware);
 // Routes init
 route(app);
 
